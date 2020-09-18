1fa7ef62a69e65353c0d82a3f7a1286939283ce4
KaterinaAtanasoska <40323665+KaterinaAtanasoska@users.noreply.github.com>
2020-09-18 17:09:40 +0200

    HomeworkClass07

---
+++ SEDC.PizzaApp.Refactored.Reworked01/SEDC.PizzaApp.Services/Implementations/PizzaService.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SEDC.PizzaApp.DataAccess;
+using SEDC.PizzaApp.Domain.Models;
+using SEDC.PizzaApp.Mappers.Pizza;
+using SEDC.PizzaApp.Services.Interfaces;
+using SEDC.PizzaApp.ViewModels.Pizza;
+
+namespace SEDC.PizzaApp.Services.Implementations
+{
+    public class PizzaService: IPizzaService
+    {
+        private IRepository<Pizza> _pizzaRepository;
+
+        public PizzaService(IRepository<Pizza> pizzaRepository)
+        {
+            _pizzaRepository = pizzaRepository ;
+        }
+
+        public void CreatePizza(PizzaViewModel pizzaViewModel)
+        {
+            Pizza pizza = pizzaViewModel.ToPizza();
+            int newPizzaId = _pizzaRepository.Insert(pizza);
+            if (newPizzaId <= 0)
+            {
+                throw new Exception("Something went wrong while saving the new pizza");
+            }
+        }
+
+        public List<PizzaViewModel> GetAllPizzas()
+        {
+            List<Pizza> pizzas = _pizzaRepository.GetAll();
+            List<PizzaViewModel> viewModels = new List<PizzaViewModel>();
+            foreach (var pizza in pizzas)
+            {
+                viewModels.Add(pizza.ToPizzaViewModel());
+            }
+
+            return viewModels;
+        }
+
+        public PizzaViewModel GetPizzaById(int id)
+        {
+            Pizza pizza = _pizzaRepository.GetById(id);
+            if (pizza == null)
+            {
+                
+                throw new Exception($"Pizza with id {id} does not exist!");
+            }
+
+            return pizza.ToPizzaViewModel();
+        }
+
+        public List<PizzaDDViewModel> GetPizzasForDropdown()
+        {
+            List<Pizza> pizzas = _pizzaRepository.GetAll();
+            List<PizzaDDViewModel> pizzaDdViewModels = new List<PizzaDDViewModel>();
+            foreach (Pizza pizza in pizzas)
+            {
+                pizzaDdViewModels.Add(pizza.ToPizzaDdViewModel());
+            }
+
+            return pizzaDdViewModels;
+        }
+    }
+}
---
+++ SEDC.PizzaApp.Refactored.Reworked01/SEDC.PizzaApp.Services/Interfaces/IPizzaService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SEDC.PizzaApp.ViewModels.Pizza;
+
+namespace SEDC.PizzaApp.Services.Interfaces
+{
+    public interface IPizzaService
+    {
+        List<PizzaViewModel> GetAllPizzas();
+        List<PizzaDDViewModel> GetPizzasForDropdown();
+        PizzaViewModel GetPizzaById(int id);
+        void CreatePizza(PizzaViewModel pizzaViewModel);
+    }
+}
