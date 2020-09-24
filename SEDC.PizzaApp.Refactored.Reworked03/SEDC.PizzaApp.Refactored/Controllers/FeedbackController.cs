0d28205b594cd1ab2bdd764e7fa452ff188a8057
KaterinaAtanasoska <40323665+KaterinaAtanasoska@users.noreply.github.com>
2020-09-24 20:38:52 +0200

    HomeworkClass10

---
+++ SEDC.PizzaApp.Refactored.Reworked03/SEDC.PizzaApp.Refactored/Controllers/FeedbackController.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SEDC.PizzaApp.Services.Interfaces;
+using SEDC.PizzaApp.ViewModels.Feedback;
+
+namespace SEDC.PizzaApp.Refactored.Controllers
+{
+    public class FeedbackController : Controller
+    {
+        private IFeedbackService _feedbackService;
+        public FeedbackController(IFeedbackService feedbackService)
+        {
+            _feedbackService = feedbackService;
+        }
+        public IActionResult Index()
+        {
+            List<FeedbackViewModel> feedbackViewModels = _feedbackService.GetAllFeedbacks();
+            return View(feedbackViewModels);
+        }
+
+        public IActionResult CreateFeedback()
+        {
+            FeedbackViewModel feedbackViewModel = new FeedbackViewModel();
+
+            return View(feedbackViewModel);
+        }
+
+        [HttpPost]
+        public IActionResult CreateFeedbackPost(FeedbackViewModel feedbackViewModel)
+        {
+            try
+            {
+                _feedbackService.CreateFeedback(feedbackViewModel);
+                return RedirectToAction("Index");
+            }
+            catch (Exception e)
+            {
+                return View("ExceptionView");
+            }
+
+        }
+        public IActionResult EditFeedback(int? id)
+        {
+            if (id == null)
+            {
+                return View("BadRequest");
+            }
+
+            try
+            {
+                FeedbackViewModel feedbackViewModel = _feedbackService.GetFeedbackForEditing(id.Value);
+
+                return View(feedbackViewModel);
+            }
+            catch
+            {
+                return View("ExceptionView");
+            }
+
+        }
+
+        [HttpPost]
+        public IActionResult EditFeedbackPost(FeedbackViewModel feedbackViewModel)
+        {
+            try
+            {
+                _feedbackService.EditFeedback(feedbackViewModel);
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View("ExceptionView");
+            }
+
+        }
+
+        public IActionResult DeleteFeedback(int? id)
+        {
+            if (id == null)
+            {
+                return View("BadRequest");
+            }
+
+            try
+            {
+                FeedbackViewModel feedbackViewModel = _feedbackService.GetFeedbackById(id.Value);
+                return View(feedbackViewModel);
+            }
+            catch
+            {
+                return View("ExceptionView");
+            }
+        }
+
+        [HttpPost]
+        public IActionResult DeleteFeedbackPost(FeedbackViewModel feedbackViewModel)
+        {
+            try
+            {
+                _feedbackService.DeleteFeedback(feedbackViewModel.Id);
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View("ExceptionView");
+            }
+        }
+    }
+}
---
+++ SEDC.PizzaApp.Refactored.Reworked03/SEDC.PizzaApp.Services/Interfaces/IFeedbackService.cs
@@ -0,0 +1,17 @@
+﻿using SEDC.PizzaApp.ViewModels.Feedback;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SEDC.PizzaApp.Services.Interfaces
+{
+    public interface IFeedbackService
+    {
+        List<FeedbackViewModel> GetAllFeedbacks();
+        FeedbackViewModel GetFeedbackById(int id);
+        void CreateFeedback(FeedbackViewModel feedbackViewModel);
+        FeedbackViewModel GetFeedbackForEditing(int id);
+        void EditFeedback(FeedbackViewModel feedbackViewModel);
+        void DeleteFeedback(int id);
+    }
+}
